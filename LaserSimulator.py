3f11f085b925ca568e6e36720fe851491380d916
Louis Georgi <louisgeorgi@LouisGeorgisMBP.local>
2017-01-08 16:56:10 +0100

    Mod: rework structure and draft of method to determine intersections between tree and laser

--- LaserSimulator.py
+++ LaserSimulator.py
@@ -14,7 +14,8 @@ class Tree(object):
         self.bhd = bhd
         self.azimuth = azimuth
         self.distance = distance
-        self.intersect = False
+
+        # self.intersect = False
 
     def get_tree_line(self, laser_vector):  # calculate tree line perpendicular to laser vector
         perpendicular_vector = np.array([-1 * laser_vector[1], laser_vector[0]])
@@ -23,13 +24,19 @@ class Tree(object):
 
         return start, end
 
+    def get_tree_coordinates(self):
+        return Point(self.distance * np.cos(self.azimuth), self.distance * np.sin(self.azimuth))
+
 
 class Laser(object):
-    def __init__(self, azimuth, distance, angular_step):
+    def __init__(self, id, azimuth, distance, angular_step):
+        self.id = id
         self.azimuth = azimuth
         self.distance = distance
         self.angular_step = angular_step
+
         self.angle = 0  # starts at angle 0 deg
+
         self.calculate_beam_vector()  # richtungsvektor
         self.vector = np.zeros(1)
 
@@ -40,19 +47,56 @@ class Laser(object):
     def calculate_beam_vector(self):
         self.vector = np.array(([np.cos(self.angle * np.pi / 180.), np.sin(self.angle * np.pi / 180.)]))
 
+    def get_laser_coordinates(self):
+        return Point(self.distance * np.cos(self.azimuth), self.distance * np.sin(self.azimuth))
+
 
 class Simulator(object):
-    def __init__(self, circle_id, tree_id, distance, azimuth, bhd):
+    def __init__(self, circle, laser):
+
+        self.circle_id = circle['circle_id']
+        self.distance = circle['distance']
+
         self.plane_diameter = 0
         self.number_of_trees = 0
         self.trees = []
         self.laser = []
+        self.intersections = []
 
-        for i in range(len(tree_id)):
-            self.trees.append(Tree(tree_id[i], bhd[i], azimuth[i], distance[i]))
+        for i in range(len(circle['tree_id'])):
+            self.trees.append(Tree(circle['tree_id'][i], circle['bhd'][i], circle['azimuth'][i], circle['distance'][i]))
 
-        self.create_laser(0, 0, 1)
-        self.create_laser(2, 3, 1)
+        for l in laser:
+            self.laser.append(Laser(l['id'], l['distance'], l['azimuth'], l['step']))
+
+    def calc_intersections(self):
+        for laser in self.laser:
+            l0 = laser.get_laser_coordinates()
+            l1 = Point(300 * np.cos(laser.angle), 300 * np.sin(laser.angle))
+            a = (l1.y - l0.y) / (l1.x - l0.x)
+            b = 1
+            c = (l0.x * (l1.y - l0.y) / (l1.x - l0.x)) + l0.y
+
+            for tree in self.trees:
+                t0 = tree.get_tree_coordinates()
+                d = c - a * t0.x - b * t0.y
+                r_squared = tree.bhd ** 2
+
+                x1 = t0.x + (a * d + b * np.sqrt(r_squared * (a ** 2 + b ** 2) - d ** 2)) / (a ** 2 + b ** 2)
+                x2 = t0.x + (a * d - b * np.sqrt(r_squared * (a ** 2 + b ** 2) - d ** 2)) / (a ** 2 + b ** 2)
+
+                y1 = t0.y + (b * d - a * np.sqrt(r_squared * (a ** 2 + b ** 2) - d ** 2)) / (a ** 2 + b ** 2)
+                y2 = t0.y + (b * d + a * np.sqrt(r_squared * (a ** 2 + b ** 2) - d ** 2)) / (a ** 2 + b ** 2)
+
+                intersections = [Point(x1, y1), Point(x2, y2)]
+
+                #if r_squared*(a**2 + b**2) == d**2: # ein schnittpunkt
+
+                #elif r_squared*(a**2 + b**2) > d**2: # zwei schnittpunkte
+
+                #else: # kein Schnittpunkt
+
+            return intersections
 
     def simulate(self):
         for laser in self.laser:
@@ -84,7 +128,7 @@ class Simulator(object):
                 # print("Tree intersects!")
                 # find nearest intersection
                 distances.append(np.sqrt((laser.position[0] - tree.position[0]) ** 2 + (
-                laser.position[1] - tree.position[1]) ** 2))
+                    laser.position[1] - tree.position[1]) ** 2))
                 tree_id.append(tree.id)
         if len(distances) > 0:
             idx = np.argmin(distances)
@@ -94,9 +138,6 @@ class Simulator(object):
             for shadow in shadowed_trees:
                 if len(shadowed_trees) > 0:
                     self.shadowed.append(shadow)
-
-    def create_laser(self, x, y, angle):
-        self.laser.append(Laser(x, y, angle))
 
     def ccw(self, A, B, C):
         return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
--- simulator_script.py
+++ simulator_script.py
@@ -15,20 +15,42 @@ def read_tree_csv(filename):
 
     # open csv file
     with open(filename, 'rb') as csvfile:
+        i = 0
+        for line in csvfile.readlines():
+            if i > 0:
+                array = line.split(';')
+                circle_id_current = int(array[0])
+                break
+            i += 1
+
+    data = []
+    # open csv file
+    with open(filename, 'rb') as csvfile:
         # get number of columns
         i = 0
         for line in csvfile.readlines():
             if i > 0:
                 array = line.split(';')
-                circle_id.append(int(array[0]))
-                tree_id.append(int(array[1]))
-                distance.append(float(array[2]))
-                azimuth.append(int(array[3]))
-                bhd.append(int(array[4]))
+                circle_id = int(array[0])
+                if circle_id == circle_id_current:
+                    tree_id.append(int(array[1]))
+                    distance.append(float(array[2]))
+                    azimuth.append(int(array[3]))
+                    bhd.append(int(array[4]))
+                else:
+                    data.append({'circle_id': circle_id_current, 'tree_id': tree_id, 'distance': distance, 'azimuth': azimuth, 'bhd': bhd})
+                    circle_id_current = int(array[0])
+                    tree_id = []
+                    distance = []
+                    azimuth = []
+                    bhd = []
+                    tree_id.append(int(array[1]))
+                    distance.append(float(array[2]))
+                    azimuth.append(int(array[3]))
+                    bhd.append(int(array[4]))
             i += 1
-        unique_circle_ids = np.unique(circle_id)
-
-    return circle_id, tree_id, distance, azimuth, bhd
+        data.append({'circle_id': circle_id_current, 'tree_id': tree_id, 'distance': distance, 'azimuth': azimuth, 'bhd': bhd})
+    return data
 
 
 def draw_plane():
@@ -58,9 +80,13 @@ def draw_plane():
     plt.show()
 
 
-circle_id, tree_id, distance, azimuth, bhd = read_tree_csv('Python_Kreise_Probe.csv')
+circles = read_tree_csv('Python_Kreise_Probe.csv')
 
-simulator = sim.Simulator(circle_id, tree_id, distance, azimuth, bhd)
+laser = [{'id': 1, 'distance': 0, 'azimuth': 0, 'step': 0.1}, {'id': 2, 'distance': 10, 'azimuth': 0, 'step': 0.1}]
+
+#for circle in circles:
+simulator = sim.Simulator(circles[0], laser)
+simulator.calc_intersections()
 #simulator.simulate()
 # simulator.test_tree_crossing(sim.trees[0])
 
